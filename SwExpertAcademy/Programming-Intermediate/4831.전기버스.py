fd70da25e6132415bf00798e58675653258844b2
Phabala <kihong2424@gmail.com>
2022-12-25 17:48:31 +0900

    4831.전기버스 (정렬(sort))

--- SwExpertAcademy/Programming-Intermediate/4831.전기버스.py
+++ SwExpertAcademy/Programming-Intermediate/4831.전기버스.py
@@ -11,10 +11,24 @@ for test_case in range(1, T + 1):
 
     count = 0
     bus = 0
-    while (bus != busStopLength):
 
-        for idx in range(bus, busStopLength):
-            pass
+    while (bus != busStopLength):  # 버스가 종점에 다다르면 while문 중단
 
-    # print('#', test_case, ' ', count, sep='')
-    # 풀다 맘
+        tmpDict = {}  # 버스와 정류장 사이 거리를 담는 딕셔너리
+        for idx in range(bus+1, busStopLength+1):  # 딕셔너리에 버스와 정류장 사이 거리를 담는 과정
+            # 충전기가 있는 정류장이거나 종점이고, enableMove 거리 안에 있다면 딕셔너리에 추가
+            if ((chargerList.count(idx) == 1) | (idx == busStopLength)) & ((idx - bus) <= enableMove):
+                tmpDict[idx] = idx - bus
+
+        # 딕셔너리가 비어있다면 버스 이동 불가 (emableMove보다 거리가 긴 정류장들은 걸러졌으므로)
+        if tmpDict == {}:
+            count = 0
+            break
+        else:
+            count += 1
+            bus = max(tmpDict, key=tmpDict.get)  # value가 가장 큰 key를 get
+            if bus == busStopLength:
+                count -= 1  # 종점은 count 갯수에서 제외
+            else:
+                pass
+    print('#', test_case, ' ', count, sep='')

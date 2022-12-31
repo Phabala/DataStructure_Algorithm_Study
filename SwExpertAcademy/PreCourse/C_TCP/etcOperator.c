203b512a428c97757f9d8e8862ece2355fbb37cd
Phabala <kihong2424@gmail.com>
2023-01-01 02:56:22 +0900

    C언어 사전학습

--- SwExpertAcademy/PreCourse/C_TCP/etcOperator.c
+++ SwExpertAcademy/PreCourse/C_TCP/etcOperator.c
@@ -21,6 +21,24 @@ int main(void)
 
     int num03 = 15, num04 = 8;
     printf("첫 번째 수는 %d이고, 두 번째 수는 %d입니다.\n", num03, num04);
-    
+
+    // sizeof 연산자 (함수가 아님)
+
+    char a;
+    int b;
+    double d;
+
+    printf("\n sizeof 연산자\n");
+
+    printf(" a = %d byte \n", sizeof(a));
+    printf(" b = %d byte \n", sizeof(b));
+    printf(" d = %d byte \n", sizeof(d));
+
+    printf("\n 시스템이 자동으로 인식하는 크기\n");
+    printf(" #      = %d byte \n", sizeof('#'));
+    printf(" 50     = %d byte \n", sizeof(50));
+    printf(" 4.153  = %d byte \n", sizeof(4.153));
+    printf(" kim    = %d byte \n", sizeof("kim"));
+
     return 0;
 }
